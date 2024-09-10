1d4e77398c62158e9c8859aa891590d0fef9749a
ArkadiuszWasilewski <wasilewski.arkadiusz96@gmail.com>
2024-09-10 15:59:15 +0200

    feat: verify firebase token

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,35 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import express, { Express } from 'express';
-import { connectDB } from './config/dbConfig';
+import cors from 'cors'
+import verifyFirebaseToken from './middleware/verifyFirebaseToken'; // Adjust path as necessary
 import userRoutes from './routes/userRoutes';
 
 // Initialize the Express application
 const app: Express = express();
 
+// Configure CORS (Cross-origin resource sharing)
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  
+
 // Middleware
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(verifyFirebaseToken);
+
 
 // Routes
 app.use('/api/users', userRoutes);
+//app.use('/verify-token', verifyToken)
+
+// Define the /verify-token route
+app.get('/verify-token', (req: any, res: express.Response) => { // Use `any` for req to access `user` property
+    console.log('User:', req.user); // Debugging log
+    res.send(`Hello, user with UID: ${req.user?.uid}`);
+    console.log("User is connecting");
+  });
 
 // Error Handling Middleware
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
---
+++ src/middleware/verifyFirebaseToken.ts
@@ -0,0 +1,31 @@
+import admin from "../config/firebaseAdmin";
+import { Request, Response, NextFunction } from 'express';
+
+interface AuthenticatedRequest extends Request {
+    user?: admin.auth.DecodedIdToken;
+}
+
+const verifyFirebaseToken = async (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+) => {
+    const idToken = req.headers.authorization?.split("Bearer ")[1];
+    console.log('Received token:', idToken); // Debugging log
+
+    if(!idToken) {
+        return res.status(401).json({message: "No token provided"});
+    }
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        req.user = decodedToken;
+        console.log("Decoded token: ", decodedToken);
+        next();
+    } catch (error) {
+        console.error('Token verification failed:', error); // Debugging log
+        return res.status(403).json({message: "Unauthorized access"});
+    }
+};
+
+export default verifyFirebaseToken;
